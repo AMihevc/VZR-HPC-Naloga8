cff72b088ba2f9813f2b134b447be869574cc8d5
AMihevc <mihevc.anze@gmail.com>
2023-04-27 21:19:05 +0200

    clean up and results

--- mpi_pi.c
+++ mpi_pi.c
@@ -65,7 +65,6 @@ int main(int argc, char *argv[])
 	if( rank != 0 ) // ločimo procese glede na rank (0 je root proces običajno)
 	{ 
 		//vsi ki niso root pošlejo root procesu svoj del PI
-        // printf("Sending count %d to root process\n", count);
         MPI_Send(&count, 1, MPI_INT, 0, rank, MPI_COMM_WORLD);
 
 
@@ -74,16 +73,14 @@ int main(int argc, char *argv[])
 	{ 
         int global_count = count;
         int recv_count = 0;
-        // printf("Global count is %d\n", count);
+
         //get count from all other processes
         for (int i = 1; i < num_p; i++)
         {
             MPI_Recv(&recv_count, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
             global_count += recv_count;
-            // printf("Receiving count %d from process %d\n", recv_count, i);
         }
 
-        // printf("Global count after recv is %d\n", global_count);
         //calculte PI
         pi = ((double)global_count / SAMPLES) * 4.0;
 
@@ -96,19 +93,183 @@ int main(int argc, char *argv[])
 
 	}
 
-    
 	MPI_Finalize(); 
 
 	return 0; 
-} 
+}
+
 /*
 1 proces: 
-
+Node name: nsc-msv007.ijs.si | rank: 0
+PI = 3.141539 | computed in 52.0203 s
+Number of processes: 1
 
 2 procesa:
 1 node:
-
+Node name: nsc-msv007.ijs.si | rank: 1
+Node name: nsc-msv007.ijs.si | rank: 0
+PI = 3.141552 | computed in 26.0245 s
+Number of processes: 2
 
 2 nodes:
+Node name: nsc-msv011.ijs.si | rank: 1
+Node name: nsc-msv007.ijs.si | rank: 0
+PI = 3.141614 | computed in 26.0202 s
+Number of processes: 2
+
+4 procesi:
+1 node:
+Node name: nsc-msv007.ijs.si | rank: 1
+Node name: nsc-msv007.ijs.si | rank: 0
+Node name: nsc-msv007.ijs.si | rank: 2
+Node name: nsc-msv007.ijs.si | rank: 3
+PI = 3.141617 | computed in 13.0356 s
+Number of processes: 4
+
+2 nodes:
+Node name: nsc-msv011.ijs.si | rank: 2
+Node name: nsc-msv011.ijs.si | rank: 3
+Node name: nsc-msv007.ijs.si | rank: 1
+Node name: nsc-msv007.ijs.si | rank: 0
+PI = 3.141562 | computed i
+
+8 procesov:
+1 node:
+Node name: nsc-msv007.ijs.si | rank: 4
+Node name: nsc-msv007.ijs.si | rank: 0
+Node name: nsc-msv007.ijs.si | rank: 1
+Node name: nsc-msv007.ijs.si | rank: 5
+Node name: nsc-msv007.ijs.si | rank: 2
+Node name: nsc-msv007.ijs.si | rank: 6
+Node name: nsc-msv007.ijs.si | rank: 7
+Node name: nsc-msv007.ijs.si | rank: 3
+PI = 3.141479 | computed in 7.9017 s
+Number of processes: 8
+
+2 nodes:
+Node name: nsc-msv011.ijs.si | rank: 5
+Node name: nsc-msv011.ijs.si | rank: 6
+Node name: nsc-msv011.ijs.si | rank: 7
+Node name: nsc-msv011.ijs.si | rank: 4
+Node name: nsc-msv007.ijs.si | rank: 2
+Node name: nsc-msv007.ijs.si | rank: 0
+Node name: nsc-msv007.ijs.si | rank: 1
+Node name: nsc-msv007.ijs.si | rank: 3
+PI = 3.141433 | computed in 6.5220 s
+Number of processes: 8
+
+16 procesov:
+1 node:
+Node name: nsc-msv007.ijs.si | rank: 12
+Node name: nsc-msv007.ijs.si | rank: 1
+Node name: nsc-msv007.ijs.si | rank: 13
+Node name: nsc-msv007.ijs.si | rank: 2
+Node name: nsc-msv007.ijs.si | rank: 0
+Node name: nsc-msv007.ijs.si | rank: 4
+Node name: nsc-msv007.ijs.si | rank: 5
+Node name: nsc-msv007.ijs.si | rank: 7
+Node name: nsc-msv007.ijs.si | rank: 9
+Node name: nsc-msv007.ijs.si | rank: 3
+Node name: nsc-msv007.ijs.si | rank: 8
+Node name: nsc-msv007.ijs.si | rank: 6
+Node name: nsc-msv007.ijs.si | rank: 15
+Node name: nsc-msv007.ijs.si | rank: 11
+Node name: nsc-msv007.ijs.si | rank: 14
+Node name: nsc-msv007.ijs.si | rank: 10
+PI = 3.141654 | computed in 3.9571 s
+Number of processes: 16
+
+2 nodes:
+Node name: nsc-msv011.ijs.si | rank: 8
+Node name: nsc-msv011.ijs.si | rank: 12
+Node name: nsc-msv011.ijs.si | rank: 9
+Node name: nsc-msv011.ijs.si | rank: 13
+Node name: nsc-msv011.ijs.si | rank: 10
+Node name: nsc-msv011.ijs.si | rank: 14
+Node name: nsc-msv011.ijs.si | rank: 11
+Node name: nsc-msv011.ijs.si | rank: 15
+Node name: nsc-msv007.ijs.si | rank: 2
+Node name: nsc-msv007.ijs.si | rank: 4
+Node name: nsc-msv007.ijs.si | rank: 6
+Node name: nsc-msv007.ijs.si | rank: 1
+Node name: nsc-msv007.ijs.si | rank: 3
+Node name: nsc-msv007.ijs.si | rank: 5
+Node name: nsc-msv007.ijs.si | rank: 0
+Node name: nsc-msv007.ijs.si | rank: 7
+PI = 3.141557 | computed in 3.9558 s
+Number of processes: 16
+
+32 procesov:
+1 node:
+Node name: nsc-msv007.ijs.si | rank: 21
+Node name: nsc-msv007.ijs.si | rank: 2
+Node name: nsc-msv007.ijs.si | rank: 8
+Node name: nsc-msv007.ijs.si | rank: 1
+Node name: nsc-msv007.ijs.si | rank: 27
+Node name: nsc-msv007.ijs.si | rank: 14
+Node name: nsc-msv007.ijs.si | rank: 17
+Node name: nsc-msv007.ijs.si | rank: 5
+Node name: nsc-msv007.ijs.si | rank: 29
+Node name: nsc-msv007.ijs.si | rank: 10
+Node name: nsc-msv007.ijs.si | rank: 9
+Node name: nsc-msv007.ijs.si | rank: 19
+Node name: nsc-msv007.ijs.si | rank: 28
+Node name: nsc-msv007.ijs.si | rank: 18
+Node name: nsc-msv007.ijs.si | rank: 22
+Node name: nsc-msv007.ijs.si | rank: 31
+Node name: nsc-msv007.ijs.si | rank: 6
+Node name: nsc-msv007.ijs.si | rank: 13
+Node name: nsc-msv007.ijs.si | rank: 30
+Node name: nsc-msv007.ijs.si | rank: 25
+Node name: nsc-msv007.ijs.si | rank: 16
+Node name: nsc-msv007.ijs.si | rank: 20
+Node name: nsc-msv007.ijs.si | rank: 11
+Node name: nsc-msv007.ijs.si | rank: 15
+Node name: nsc-msv007.ijs.si | rank: 24
+Node name: nsc-msv007.ijs.si | rank: 12
+Node name: nsc-msv007.ijs.si | rank: 23
+Node name: nsc-msv007.ijs.si | rank: 4
+Node name: nsc-msv007.ijs.si | rank: 0
+Node name: nsc-msv007.ijs.si | rank: 26
+Node name: nsc-msv007.ijs.si | rank: 3
+Node name: nsc-msv007.ijs.si | rank: 7
+PI = 3.141649 | computed in 2.8574 s
+Number of processes: 32
+
+2 nodes:
+Node name: nsc-msv007.ijs.si | rank: 3
+Node name: nsc-msv007.ijs.si | rank: 9
+Node name: nsc-msv007.ijs.si | rank: 12
+Node name: nsc-msv007.ijs.si | rank: 13
+Node name: nsc-msv007.ijs.si | rank: 7
+Node name: nsc-msv007.ijs.si | rank: 11
+Node name: nsc-msv007.ijs.si | rank: 15
+Node name: nsc-msv007.ijs.si | rank: 2
+Node name: nsc-msv007.ijs.si | rank: 6
+Node name: nsc-msv007.ijs.si | rank: 10
+Node name: nsc-msv007.ijs.si | rank: 14
+Node name: nsc-msv007.ijs.si | rank: 1
+Node name: nsc-msv007.ijs.si | rank: 5
+Node name: nsc-msv007.ijs.si | rank: 8
+Node name: nsc-msv007.ijs.si | rank: 0
+Node name: nsc-msv007.ijs.si | rank: 4
+Node name: nsc-msv011.ijs.si | rank: 28
+Node name: nsc-msv011.ijs.si | rank: 24
+Node name: nsc-msv011.ijs.si | rank: 19
+Node name: nsc-msv011.ijs.si | rank: 23
+Node name: nsc-msv011.ijs.si | rank: 29
+Node name: nsc-msv011.ijs.si | rank: 26
+Node name: nsc-msv011.ijs.si | rank: 30
+Node name: nsc-msv011.ijs.si | rank: 16
+Node name: nsc-msv011.ijs.si | rank: 20
+Node name: nsc-msv011.ijs.si | rank: 27
+Node name: nsc-msv011.ijs.si | rank: 31
+Node name: nsc-msv011.ijs.si | rank: 17
+Node name: nsc-msv011.ijs.si | rank: 21
+Node name: nsc-msv011.ijs.si | rank: 25
+Node name: nsc-msv011.ijs.si | rank: 18
+Node name: nsc-msv011.ijs.si | rank: 22
+PI = 3.141573 | computed in 2.2221 s
+Number of processes: 32
 
 */
